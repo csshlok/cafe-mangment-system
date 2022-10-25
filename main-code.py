fc979a08e296b10586363b1950770eab54be9c0e
Shlok Dwivedi <84954530+csshlok@users.noreply.github.com>
2022-10-25 22:17:28 +0530

    updated main code

--- main-code.py
+++ main-code.py
@@ -4,7 +4,7 @@ import mysql.connector
 mydb = mysql.connector.connect(host='localhost', user='root', password='Ggn@0124', database='project')
 import datetime
 import csv
-from PIL import image
+from PIL import Image
 
 ###CODE START###
 ########MENU#########
